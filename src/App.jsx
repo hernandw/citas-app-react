c43b3e3857b1f16868c46bf05923489e04108cd6
Williams Hernandez <hernandw@gmail.com>
2023-02-09 19:46:50 -0300

    :tada: inicio del proyecto y configuraciones

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Formulario from './components/Formulario'
+import Header from './components/Header'
+import ListadoPacientes from './components/ListadoPacientes'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Formulario />
+    <ListadoPacientes />
+    </>
+  )
+}
+
+export default App
