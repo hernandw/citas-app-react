ba45ecc1a2a500ceea32f28d98b9bce33efe1856
Williams Hernandez <hernandw@gmail.com>
2023-02-10 16:10:19 -0300

    :sparkles: agregar botones de editar y eliminar

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,15 +4,33 @@ import Paciente from "./Paciente";
 const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 h-screen md:overflow-y-scroll p-2">
-      <h2 className=" font-bold text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-lg mt-5 mb-10 text-center">
-        Administra tus{" "}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas </span>
-      </p>
-      {pacientes.map((paciente) => (
-        <Paciente key={paciente.id} paciente={paciente}
-        />
-      ))}
+      {pacientes.length === 0 ? (
+        <>
+          <h2 className=" font-bold text-3xl text-center">No hay pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Comienza agregando pacientes{" "}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar
+            </span>
+          </p>
+
+          
+        </>
+      ) : (
+        <>
+          <h2 className=" font-bold text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-lg mt-5 mb-10 text-center">
+            Administra tus{" "}
+            <span className="text-indigo-600 font-bold">
+              Pacientes y Citas{" "}
+            </span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 
-const Paciente = ({paciente}) => {
- const { name, owner, alta, email, symptoms }= paciente
+const Paciente = ({ paciente }) => {
+  const { name, owner, alta, email, symptoms } = paciente;
   return (
     <div className="md:ml-4 bg-white shadow-md px-5 py-10 rounded-xl mb-4">
-        <p className="font-bold mb-3 text-gra uppercase">
-          Nombre: {""} <span className="font-normal normal-case">{name}</span>
-        </p>
-        <p className="font-bold mb-3 text-gra uppercase">
-          Propietario: {""} <span className="font-normal normal-case">{owner}</span>
-        </p>
-        <p className="font-bold mb-3 text-gra uppercase">
-          Email: {""} <span className="font-normal normal-case">{email}</span>
-        </p>
-        <p className="font-bold mb-3 text-gra uppercase">
-          Alta: {""} <span className="font-normal normal-case">{alta}</span>
-        </p>
-        <p className="font-bold mb-3 text-gra uppercase">
-          Sintomas: {""} <span className="font-normal normal-case">{symptoms}</span>
-        </p>
-        
-      </div>
-  )
-}
+      <p className="font-bold mb-3 text-gra uppercase">
+        Nombre: {""} <span className="font-normal normal-case">{name}</span>
+      </p>
+      <p className="font-bold mb-3 text-gra uppercase">
+        Propietario: {""}{" "}
+        <span className="font-normal normal-case">{owner}</span>
+      </p>
+      <p className="font-bold mb-3 text-gra uppercase">
+        Email: {""} <span className="font-normal normal-case">{email}</span>
+      </p>
+      <p className="font-bold mb-3 text-gra uppercase">
+        Alta: {""} <span className="font-normal normal-case">{alta}</span>
+      </p>
+      <p className="font-bold mb-3 text-gra uppercase">
+        Sintomas: {""}{" "}
+        <span className="font-normal normal-case">{symptoms}</span>
+      </p>
 
-export default Paciente
+      <div className="flex justify-between pt-5">
+      <button className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg">Editar</button>
+      <button className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg">Eliminar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Paciente;
